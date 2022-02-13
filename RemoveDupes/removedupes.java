83eff57a84f7519f7a6cf97b54d0564396b1ba99
Fahim Bashar <89543299+tuk93846@users.noreply.github.com>
2022-02-12 20:32:22 -0500

    removedupes.java added

--- RemoveDupes/removedupes.java
+++ RemoveDupes/removedupes.java
@@ -4,7 +4,33 @@ public class removedupes {
     class Solution {
         public int removeDuplicates(int[] nums) 
         {
-            return 0;
+
+            //case of nums being an empty array
+            if(nums.length == 0)
+            {
+                return 0;
+            }
+
+            //if there are values in nums
+            else
+            {
+                //creating pointer i
+                int i = 0;
+
+                //creating pointer j
+                for(int j = 1; j < nums.length; j++)
+                {
+                    //j goes thru the whole array
+                    if(nums[j] != nums[i])
+                    {
+                        i++;
+                        //i only updates when a new value is pointed at
+                        nums[i] = nums[j];
+                    }
+                }
+                //unique value at nums[0] not counted so we add one
+                return i + 1;
+            }
         }
     }
 }
